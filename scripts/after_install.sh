304482d2938b6bceb229a50b50d5e379d5dd7a85
praatikgote <pratikgote69@gmail.com>
2023-02-19 12:26:33 +0530

    sameas privious

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ cd /home/ec2-user/server
 
 #_Remove_Unused_Code
 rm -rf node_modules
-#_rm -rf build ------
+rm -rf build ------
 
 #Install_node_modules_&_Make_React_Build
 npm  install
-#_npm run build ------
+npm run build ------
