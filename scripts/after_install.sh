fbe274b7294a89bf86855ec3beed483d86a4a731
praatikgote <pratikgote69@gmail.com>
2023-02-19 13:20:43 +0530

    123

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ cd /home/ec2-user/server
 
 #_Remove_Unused_Code
 rm -rf node_modules
-rm -rf build ------
+rm -rf build 
 
 #Install_node_modules_&_Make_React_Build
 npm  install
-#npm run build ------
+npm run build 
