b825afe57d5e3f1cd8825389e765e6cff9029098
praatikgote <pratikgote69@gmail.com>
2023-02-20 12:04:51 +0530

    pratik gote

--- middleware/pakageAuth.js
+++ middleware/pakageAuth.js
@@ -2,7 +2,7 @@ exports.authenticatePakage = (req, res, next) => {
     console.log(req.headers.authorization)
   if (!req.headers.authorization) {
     return res.status(401).json({
-      message: "you are not authorised to access pratik ***"
+      message: "you are not authorised to access pratik Gote"
     });
 }
     try {
