b83dd4bae31cb8918e8ecc042a08274f9fd0ddee
praatikgote <pratikgote69@gmail.com>
2023-02-28 18:39:42 +0530

    ch

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ec2-user/server2
+cd /home/ec2-user/server
 
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Backend
 #pm2 delete Frontend
-pm2 start -f server.js 
+pm2 start server.js 
 #pm2 start -f scripts/app_start.sh
 
 
